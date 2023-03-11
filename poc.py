129d3ebf50844ac4d2f4571cf186b09f1c19ecc8
Hongbao <hongbao_ye@trendmicro.com>
2023-03-11 14:51:42 +0800

    poc

--- poc.py
+++ poc.py
@@ -1,55 +1,26 @@
-import openai
+from revChatGPT.V1 import Chatbot
 import os
 
-# Set the API key
-openai.api_key = os.getenv("OPENAI_API_KEY")
-
-# Choose a model
-MODEL_ENGINE = "text-davinci-003"
-
-# Set the initial prompt to include a personality and habits
-INITIAL_PROMPT = ('''
-    I am a friendly artificial intelligence.
-''')
-conversation_history = INITIAL_PROMPT + "\n"
-
-USERNAME = "USER"
-# AI_NAME = "AI"
+chatbot = Chatbot(
+    config={
+        "access_token": f"{os.getenv('ChatGPT_TOKEN')}"
+    }, conversation_id=f"{os.getenv('conv_id')}"
+)
 
 
-def get_response(prompt):
-    """Returns the response for the given prompt using the OpenAI API."""
-    completions = openai.Completion.create(
-        engine=MODEL_ENGINE,
-        prompt=prompt,
-        max_tokens=1024,
-        temperature=0.7,
-    )
-    return completions.choices[0].text
+def ask_chatgpt(chatbot, prompt):
+    response = ""
+
+    for data in chatbot.ask(
+        prompt
+    ):
+        response = data["message"]
+    return response
 
 
-def handle_input(
-    prompt: str,
-    conversation_history: str,
-    USERNAME: str
-):
-    """Updates the conversation history and generates a response using GPT-3."""
-    # Update the conversation history
-    conversation_history += f"{USERNAME}: {prompt}\n"
+query = "Hello?"
 
-    # Generate a response using GPT-3
-    message = get_response(conversation_history)
+response = ask_chatgpt(chatbot, query)
+# response = chatbot.get_conversations()
 
-    # Update the conversation history
-    conversation_history += f"{message}\n"
-
-    # Print the response
-    print(f'This is response: {message}')
-
-    return conversation_history
-
-
-while True:
-    prompt = input(f"{USERNAME}: ")
-    
-    conversation_history = handle_input(prompt, conversation_history, USERNAME)
+print(response)
